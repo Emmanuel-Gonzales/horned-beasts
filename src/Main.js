97cd0a0c3ef70d293093f1a568cc33f24a88b933
Emmanuel Gonzales <emmanuelgonzales2002@gmail.com>
2023-03-14 21:18:29 -0500

    add photos

--- src/Main.js
+++ src/Main.js
@@ -6,8 +6,8 @@ class Main extends React.Component {
     return (
       <>
       <h2>Beasts</h2>
-      <Beast title="Goat" imgUrl="public/Baby-Goat.jpg" alt="Cute Baby Goat" description="Enjoy This Chill Goat Relaxing"/>
-      <Beast title="Ram" imgUrl="src/Ram.jpg" alt="Stoic Ram" description="Enjoy This Picture of a Powerful Ram" />
+      <Beast title="Goat" imgUrl="/Baby-Goat.jpg" alt="Cute Baby Goat" description="Enjoy This Chill Goat Relaxing"/>
+      <Beast title="Ram" imgUrl="/Ram.jpg" alt="Stoic Ram" description="Enjoy This Picture of a Powerful Ram" />
       </>
     )
   }
