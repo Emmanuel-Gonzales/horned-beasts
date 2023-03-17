15ceada15e765a531b0dd8c81575f86f9be2fa8d
Emmanuel Gonzales <emmanuelgonzales2002@gmail.com>
2023-03-17 11:47:27 -0500

    fix problem with lab 01

--- src/App.js
+++ src/App.js
@@ -44,11 +44,31 @@ handleSelect = (event) => {
   let selected = event.target.value;
 
   if(selected === "# of Horns"){
-    let hornedData  = data.sort((a, b) => a.horns - b.horns);
+    let hornedData = data.sort((a, b) => a.horns - b.horns);
     this.setState({
       sortedData: hornedData
     });
-  } 
+  } else if (selected === "1"){
+    let oneHorn = data.filter(beast => beast.horns === 1)
+    this.setState({
+      sortedData: oneHorn
+    });
+  } else if (selected === "2"){
+    let twoHorns = data.filter(beast => beast.horns === 2)
+    this.setState({
+      sortedData: twoHorns
+    });
+  } else if (selected === "3"){
+    let threeHorns = data.filter(beast => beast.horns === 3)
+    this.setState({
+      sortedData: threeHorns
+    });
+  } else if (selected === "100"){
+    let hundredHorn = data.filter(beast => beast.horns === 100)
+    this.setState({
+      sortedData: hundredHorn
+    });
+  }
 }
 
   render(){
@@ -59,7 +79,11 @@ handleSelect = (event) => {
         <Form.Label>Select Format</Form.Label>
         <Form.Select  name="selected" onChange={this.handleSelect}>
           <option value="">Choose</option>
-          <option value="# of Horns">Number of Horns</option>
+          <option value="# of Horns">Descending Number of Horns</option>
+          <option value="1">1 Horn</option>
+          <option value="2">2 Horns</option>
+          <option value="3">3 Horns</option>
+          <option value="100">100 Horns</option>
         </Form.Select>
       </Form.Group>
       <Main sortedData={this.state.sortedData} handleOpenModal={this.handleOpenModal} />
